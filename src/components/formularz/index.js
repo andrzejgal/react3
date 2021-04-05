7ea63ef82cb01963ebfe9b8a9ad69588bb2683d7
andrzejgal <andrzejmg@op.pl>
2021-04-05 12:13:17 +0200

    commented line out were deleted

--- src/components/formularz/index.js
+++ src/components/formularz/index.js
@@ -1,28 +1,14 @@
 import React from "react";
-//usuwamy props
-// const Form = () => {
-//     return (
-//         <form>
-//             <label>Imiê:</label>
-//             <input name="name" type="text" value={this.state.value} />
-//             <input name="send" type="submit" />
-//         </form>
-//     )
-// }
-
-/* 
-Komponent funkcyjny nie mo¿e mieæ this, wobec czego przenosimy formê do komponentu klasowego
-*/
 
 
 
 class Form extends React.Component {
-    constructor(props) {    //ustawiamy wstêpnie value
+    constructor(props) {    //ustawiamy wstï¿½pnie value
         super(props);
-        this.state = { value: 'Piotr' };  //jeœli value jest puste, to nie mo¿emy nic wpisaæ do inputa
-        //jeœli wpiszemy np. Piotr do value, to nie mo¿na go zmieniæ.
-        //dziêki temu mo¿emy tworzyæ formularze, które maj¹ niezmialne pola, np user.
-        //Aby mo¿na by³o zmienic, to mo¿emy wstawiæ jako default lub u¿yæ onchange
+        this.state = { value: 'Piotr' };  //jeï¿½li value jest puste, to nie moï¿½emy nic wpisaï¿½ do inputa
+        //jeï¿½li wpiszemy np. Piotr do value, to nie moï¿½na go zmieniï¿½.
+        //dziï¿½ki temu moï¿½emy tworzyï¿½ formularze, ktï¿½re majï¿½ niezmialne pola, np user.
+        //Aby moï¿½na byï¿½o zmienic, to moï¿½emy wstawiï¿½ jako default lub uï¿½yï¿½ onchange
         this.handleChange=this.handleChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
     }
@@ -30,7 +16,7 @@ class Form extends React.Component {
         this.setState({ value: e.target.value });
     }
 /*
-Aby formularz nie wysyla³ siê automatycznie, trzeba zatrzymaæ formularz przed wyslaniem
+Aby formularz nie wysylaï¿½ siï¿½ automatycznie, trzeba zatrzymaï¿½ formularz przed wyslaniem
 */
 handleSubmit(e) {
     e.preventDefault();
@@ -38,7 +24,7 @@ handleSubmit(e) {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>Imiê:</label>
+                <label>Imiï¿½:</label>
                 <input name="name" type="text" value={this.state.value} onChange={this.handleChange}/>
                 <input name="send" type="submit" />
             </form>
