708b541f1f3bc918fa89b31f8d2abe0fa2927afa
andrzejgal <andrzejmg@op.pl>
2021-04-05 20:32:51 +0200

    comments were deleted

--- src/components/formularz/index.js
+++ src/components/formularz/index.js
@@ -3,54 +3,48 @@ import React from "react";
 
 
 class Form extends React.Component {
-    constructor(props) {    //ustawiamy wst�pnie value
+    constructor(props) {    
         super(props);
         this.state = {
             name: 'Andrzej',
             str: generateRandom(),
             hp: generateRandom(),
             speed: generateRandom(),
-        };  //je�li value jest puste, to nie mo�emy nic wpisa� do inputa
-        //je�li wpiszemy np. Piotr do value, to nie mo�na go zmieni�.
-        //dzi�ki temu mo�emy tworzy� formularze, kt�re maj� niezmialne pola, np user.
-        //Aby mo�na by�o zmienic, to mo�emy wstawi� jako default lub u�y� onchange
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(e) {
         this.setState({ name: e.target.value });
     }
-    /*
-    Aby formularz nie wysyla� si� automatycznie, trzeba zatrzyma� formularz przed wyslaniem
-    */
+
     handleSubmit(e) {
         e.preventDefault();
     }
     render() {
-        // formularz ma zawierać:
-        // imię
-        // siła (str) wartość losowa
-        // moc (hp) wartość losowa
-        // prędkośc (speed) wartość losowa
         return (
             <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label>Imię:</label>
-                    <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                </div>
-                <div>
-                    <label>Siła:</label>
-                    <input name="str" type="text" value={this.state.str} />
-                </div>
-                <div>
-                    <label>Moc:</label>
-                    <input name="hp" type="text" value={this.state.hp} />
-                </div>
-                <div>
-                    <label>Prędkość:</label>
-                    <input name="sppeed" type="text" value={this.state.speed} />
-                </div>
-                <input name="send" type="submit" />
+                <table>
+                    <tr>
+                        <td><label>Imię:</label></td>
+                        <td><input name="name" type="text" value={this.state.name} onChange={this.handleChange} /></td>
+                    </tr>
+                    <tr>
+                        <td><label>Siła:</label></td>
+                        <td><input name="str" type="text" value={this.state.str} /></td>
+                    </tr>
+                    <tr>
+                        <td><label>Moc:</label></td>
+                        <td><input name="hp" type="text" value={this.state.hp} /></td>
+                    </tr>
+                    <tr>
+                        <td><label>Prędkość:</label></td>
+                        <td><input name="speed" type="text" value={this.state.speed} /></td>
+                    </tr>
+                    <tr>
+                        <input name="send" type="submit" />
+                    </tr>
+                </table>
             </form>
         )
     }
