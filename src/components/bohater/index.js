f18632a3c8b91267f3c8b9df67ab371cf8ef4fa1
andrzejgal <andrzejmg@op.pl>
2021-04-07 12:52:44 +0200

    elements were added  to Bohater

--- src/components/bohater/index.js
+++ src/components/bohater/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Bohater=(props)=> {
-    constructor(props) {
-        super(props);
-        this.state={show:false};
-    }
+const Bohater=()=> {
+    const { name, str, hp, speed} = useContext(CharsStats);
+    return (
+            <div >
+                {/* <h1>{props.poziom}</h1> */}
+                <p>Name: {name}</p>
+                <p>Str: {str}</p>
+                <p>hp" {hp}</p>
+                <p>speed: {speed}</p>
+             </div>
+        )
+    
 
 }
+
+export default Bohater;
 
