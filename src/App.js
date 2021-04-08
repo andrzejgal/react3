80d6f6b3759e318d2a64b62016af2bb1d9218058
andrzejgal <andrzejmg@op.pl>
2021-04-08 13:41:06 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Form  from './components/formularz';
+import Bohater from './../src/components/bohater';
 
 function App() {
   return (
+    <div>
     <Form/>
+    <Bohater/>
+    </div>
   );
 }
 
