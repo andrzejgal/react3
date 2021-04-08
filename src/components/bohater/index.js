8f1035241c892c0aec84bfcd21fb916e98619040
andrzejgal <andrzejmg@op.pl>
2021-04-08 13:41:26 +0200

    ok

--- src/components/bohater/index.js
+++ src/components/bohater/index.js
@@ -8,28 +8,47 @@ import React, { useState } from 'react';
 //     const hp = localStorage.getItem("hp");
 //     const speed = localStorage.getItem("speed");
 
+const Bohater = (props) => {
+    const [validity,setValidity]=useState(true);
+    // convertToBool(props)
+    // const name=localStorage.getItem("name");
+    return (
+        <div>
+            {
+                validity ?
+                    <div>
+                        < p > Name: "Andrzej"</p >
+                        <p>Str: {localStorage.getItem("str")}</p>
+                        <p>hp:{localStorage.getItem("hp")}</p>
+                        <p>speed: {localStorage.getItem("speed")}</p>
+                    </div >
+                    : <div>to formularz</div>
+            }</div>
+    )
+
+
 }
 
 
-const Bohater = () => {
-    // const { name, str, hp, speed} = useContext(CharsStats);
-    [validity, setValidity] = setState(false);
-    return (
-        {
-            validity ?
-                <div>
-                < p > Name: { localStorage.getItem("name"); }</p >
-                <p>Str: { localStorage.getItem("str"); }</p>
-                <p>hp:{ localStorage.getItem("hp"); }</p>
-                <p>speed: { localStorage.getItem("speed"); }</p>
-             </div >
-             : <div>to formularz</div>
-        }
-)
+// const Bohater = () => {
+//     // const { name, str, hp, speed} = useContext(CharsStats);
+//     [validity, setValidity] = setState(false);
+//     return (
+//         {
+//             validity ?
+//                 <div>
+//                 < p > Name: { localStorage.getItem("name"); }</p >
+//                 <p>Str: { localStorage.getItem("str"); }</p>
+//                 <p>hp:{ localStorage.getItem("hp"); }</p>
+//                 <p>speed: { localStorage.getItem("speed"); }</p>
+//              </div >
+//              : <div>to formularz</div>
+//         }
+// )
 
     
 
-}
+// }
 
 export default Bohater;
 
